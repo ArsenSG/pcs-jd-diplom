f4cb9e332e0bb2182e618b538d794b59cb788552
Арсен Сулейманов <ars.suleimanov@yandex.ru>
2022-07-13 19:46:34 +0300

    Sorting upd

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    private final Map<PageEntry, String> allPdfs = new HashMap<>();
+    private final Map<String, List<PageEntry>> allPdfs = new HashMap<>();
 
-    public BooleanSearchEngine(File pdfsDir) throws IOException {
-        File[] files = pdfsDir.listFiles();
+    public BooleanSearchEngine(File srcDir) throws IOException {
+        File[] files = srcDir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.getName().endsWith(".pdf")) {
@@ -25,9 +25,12 @@ public class BooleanSearchEngine implements SearchEngine {
                                 if (word.isEmpty()) {
                                     continue;
                                 }
-                                int num = freqs.getOrDefault(word, 0) + 1;
-                                freqs.put(word.toLowerCase(), num);
-                                allPdfs.put(new PageEntry(file.getName(), doc.getPageNumber(page), num), word.toLowerCase());
+                                freqs.put(word.toLowerCase(),
+                                        freqs.getOrDefault(word.toLowerCase(), 0) + 1);
+                            }
+                            for (var entry : freqs.entrySet()) {
+                                PageEntry pageEntry = new PageEntry(file.getName(), i, entry.getValue());
+                                sorting(allPdfs, entry.getKey(), pageEntry);
                             }
                         }
                     }
@@ -36,15 +39,14 @@ public class BooleanSearchEngine implements SearchEngine {
         }
     }
 
-    @Override
+    private void sorting(Map<String, List<PageEntry>> allPdfs, String word, PageEntry pageEntry) {
+        List<PageEntry> pdfList = allPdfs.getOrDefault(word, new ArrayList<>());
+        pdfList.add(pageEntry);
+        allPdfs.put(word, pdfList);
+        allPdfs.values().forEach(Collections::sort);
+    }
+
     public List<PageEntry> search(String word) {
-        var list = new ArrayList<PageEntry>();
-        for (Map.Entry<PageEntry, String> entry : allPdfs.entrySet()) {
-            if (entry.getValue().equals(word)) {
-                list.add(entry.getKey());
-            }
-        }
-        Collections.sort(list);
-        return list;
+        return allPdfs.get(word.toLowerCase());
     }
 }
